59470d5275b6257c7162cbd2b33526a04cfbd760
CJW-MAPU <c.j.biz.woong@gmail.com>
2021-01-24 16:59:58 +0900

    #refactor (Sections, Modules) CircleCard, CircleGroup, Merit & #feature (Sections) Activities

---
+++ client/components/sections/Activities/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.section`
+
+`;
+
+export const DescWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  background-color: red;
+`;
