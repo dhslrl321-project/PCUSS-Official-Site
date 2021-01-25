e6f322f9592021cbb20fc1c0159676a74bbff79b
CJW-MAPU <c.j.biz.woong@gmail.com>
2021-01-25 21:12:39 +0900

    #hotfix: (Section) Container Style 정렬 추가

--- client/components/modules/CircleCard/styles.js
+++ client/components/modules/CircleCard/styles.js
@@ -31,7 +31,7 @@ export const DescColumn = styled.div`
   width: 250px;
   margin-top: 20px;
   text-align: center;
-  ${ifProp("isVisible", "", "display: none;")}
+  display: ${ifProp("isVisible", "block;", "none;")};
 
   @media screen and (max-width: 768px) {
     width: 180px;
