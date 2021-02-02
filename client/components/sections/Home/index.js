c28591e4a1e90e051949f74bff4a68076e590d29
dhslrl321 <dhslrl321@gmail.com>
2021-02-02 16:47:33 +0900

    feature Home 컴포넌트 약식 개발 완료

---
+++ client/components/sections/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import * as S from "./styles";
+
+const Home = () => {
+  return (
+    <S.Container>
+      <S.TitleWrapper>배재대학교</S.TitleWrapper>
+      <S.SubtitleWrapper>SW 공학부 정보보안학과</S.SubtitleWrapper>
+    </S.Container>
+  )
+}
+
+export default Home
