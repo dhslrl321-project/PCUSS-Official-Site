2de2b6e8259816ad84f933358a6e12571e870b78
dhslrl321 <dhslrl321@gmail.com>
2021-02-03 20:59:36 +0900

    feature: Sidebar 구현 환료

---
+++ client/components/modules/Sidebar/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import * as S from "./styles";
+import Link from "../../atoms/Link";
+import { FaTimes } from "react-icons/fa";
+const Sidebar = ({ isOpen, toggle }) => {
+  return (
+    <S.Container isOpen={isOpen}>
+      <S.Icon>
+        <FaTimes />
+      </S.Icon>
+      <S.SidebarWrapper>
+        <S.SidebarMenu>
+          <Link styleType="NextLink" href="/">Home</Link>
+          <Link styleType="NextLink" href="/">커리큘럼</Link>
+          <Link styleType="NextLink" href="/">졸업 후 진로</Link>
+          <Link styleType="NextLink" href="/">선배의 인터뷰</Link>
+          <Link styleType="NextLink" href="/">CTF</Link>
+          <Link styleType="NextLink" href="/">세미나</Link>
+          <Link styleType="NextLink" href="/">대외 활동</Link>
+          <Link styleType="NextLink" href="/">교수진</Link>
+          <Link styleType="NextLink" href="/">랩실</Link>
+          <Link styleType="NextLink" href="/">소모임</Link>
+        </S.SidebarMenu>
+      </S.SidebarWrapper>
+    </S.Container>
+  )
+}
+
+export default Sidebar
---
+++ client/components/modules/Sidebar/styles.js
@@ -0,0 +1,60 @@
+import styled from 'styled-components';
+import { theme } from 'styled-tools';
+
+export const Container = styled.aside`
+  position: fixed;
+  z-index: 999;
+  height: 100%;
+  width: 100%;
+  background: #0d0d0d;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  top: 0;
+  left: 0;
+  transition: 0.3s ease-in-out;
+  opacity: ${({ isOpen }) => (isOpen ? "100%" : "0")};
+  top: ${({ isOpen }) => (isOpen ? "0" : '-100%')};
+`;
+
+export const Icon = styled.div`
+  position: absolute;
+  top: 1.2rem;
+  right: 1.5rem;
+  background: transparent;
+  font-size: 2rem;
+  cursor: pointer;
+  outline: none;
+`;
+
+export const SidebarWrapper = styled.div`
+  color: #fff;
+`;
+
+export const SidebarMenu = styled.ul`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  a {
+    margin: 20px;
+  }
+`;
+
+export const SidebarLink = styled.li`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1.5rem;
+  text-decoration: none;
+  list-style: none;
+  transition: 0.2s ease-in-out;
+  text-decoration: none;
+  color: #fff;
+  cursor: pointer;
+
+  &:hover {
+    color: #01bf71;
+    transition: 0.2s ease-in-out;
+  }
+`;
