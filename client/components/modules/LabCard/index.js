c38f35d95a2c601b0e67ed37b8d1cdeba32b1153
dhslrl321 <dhslrl321@gmail.com>
2021-02-22 09:29:42 +0900

    feature: Lab Component Add

---
+++ client/components/modules/LabCard/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import * as S from "./styles";
+import LabInfoCard from '../LabInfoCard'
+import { Image } from '../../atoms/Image/style'
+import Label from '../../atoms/Label';
+
+const LabCard = ({ data }) => {
+  const { labMainData, labInfoData } = data;
+  return (
+    <S.Container>
+      <S.ImageColumn>
+        <Image src={labMainData.src} />
+        <S.Description>
+          <Label styleType="Label" size="1">{labMainData.labName}</Label>
+          <Label styleType="Label" size="0.8">{labMainData.labBirth}</Label>
+          <Label styleType="Label" size="0.8">{labMainData.labLocation}</Label>
+        </S.Description>
+      </S.ImageColumn>
+      <S.InfoColumn>
+        {labInfoData.map(infoData =>
+          <LabInfoCard data={infoData} />)}
+      </S.InfoColumn>
+    </S.Container>
+  )
+}
+
+export default LabCard
---
+++ client/components/sections/Lab/styles.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+import { theme } from 'styled-tools';
+export const Container = styled.section`
+  ${theme("shortcuts.flexCenterColumn")};
+`;
