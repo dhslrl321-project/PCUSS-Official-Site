de332673e92475cba90f551f1c6fee8d450e1edf
dhslrl321 <dhslrl321@gmail.com>
2021-03-07 00:04:07 +0900

    refactor: InterviewData 파일 구조 분해

--- client/components/modules/QnACard/styles.js
+++ client/components/modules/QnACard/styles.js
@@ -4,8 +4,14 @@ import { theme } from 'styled-tools';
 export const Container = styled.div`
   ${theme("shortcuts.flexCenterColumn")};
   align-items: center;
+  span {
+    line-height: 130%;
+  }
   p {
     margin-top: 10px;
-    font-weight: 300;
+    font-weight: 200;
+    line-height: 180%;
+    color: #a2a2a2;
   }
+  margin-top: 25px;
 `;
--- client/datas/InterviewData.js
+++ client/datas/InterviewData.js
@@ -5,12 +5,12 @@ export const data = {
   },
   cardData: {
     cards: [
-      { id: 1, label: "보안 컨설팅 전문가 이재랑", src: "/interview/Interview1.JPG" },
-      { id: 2, label: "정보보안 담당자 박지훈", src: "/interview/Interview2.JPG" },
-      { id: 3, label: "모의해킹 전문가 김도언", src: "/interview/Interview3.JPG" },
-      { id: 4, label: "보안 솔루션 개발자 송태기", src: "/interview/Interview4.jpg" },
-      { id: 5, label: "보안 솔루션 운영 김영수", src: "/interview/Interview5.jpg" },
-      { id: 6, label: "보안 전문가 윤정호", src: "/interview/Interview6.jpg" },
+      { id: 1, label: "모의해킹 전문가 김도언", src: "/interview/Interview1.JPG" },
+      { id: 2, label: "보안 솔루션 개발자 송태기", src: "/interview/Interview2.JPG" },
+      { id: 3, label: "네트워크 취약점 대응 전문가 윤정호", src: "/interview/Interview3.JPG" },
+      { id: 4, label: "정보보안 컨설팅 전문가 이재랑", src: "/interview/Interview4.jpg" },
+      { id: 5, label: "리버싱 엔지니어 박지훈", src: "/interview/Interview5.jpg" },
+      { id: 6, label: "DevOps 전문가 김화림", src: "/interview/Interview6.jpg" },
     ]
   }
 };
