a75c632ac46adf72b556a693144a0a7be5632168
hhjin015 <heo015414@gmail.com>
2021-06-12 23:24:28 +0900

    #refactor generateOauthLink

--- client/components/molecules/SnsButton/index.js
+++ client/components/molecules/SnsButton/index.js
@@ -7,36 +7,14 @@ import Label from "../../atoms/Label";
 import Button from "../../atoms/Button";
 import Image from "../../atoms/Image";
 
+import { generateOauthLink } from "../../../utils/generateOauthLink";
+
 const SnsButton = ({ snsType }) => {
-  const kakaoOauthLogin = () => {
-    const client_id = "ae445ce0b4254d726918cd93c69d199c";
-    const redirect_uri = "http://localhost:3000/oauth/kakao";
-    const response_type = "code";
-
-    location.href = `https://kauth.kakao.com/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=${response_type}`;
-  };
-
-  const naverOauthLogin = () => {
-    const client_id = "zxs_Uxd9VYn17b9Aa9oW";
-    const redirect_uri = "http://localhost:3000/oauth/naver";
-    const response_type = "code";
-    const state = "01234567890123456789";
-
-    location.href = `https://nid.naver.com/oauth2.0/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=${response_type}&state=${state}`;
-  };
-
-  const googleOauthLogin = () => {
-    const client_id =
-      "724675302887-elk3qo3raci5e7q8rl00hf8n53l58djm.apps.googleusercontent.com";
-    const redirect_uri = "http://localhost:3000/oauth/google";
-    const response_type = "code";
-    const scope = "profile";
-
-    location.href = `https://accounts.google.com/o/oauth2/v2/auth?scope=${scope}&client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=${response_type}`;
+  const oauthLink = () => {
+    location.href = generateOauthLink(snsType);
   };
 
   let icon;
-  let clickType;
 
   if (snsType === "Kakao") {
     icon = (
@@ -44,26 +22,23 @@ const SnsButton = ({ snsType }) => {
         <RiKakaoTalkFill />
       </S.IconColumn>
     );
-    clickType = kakaoOauthLogin;
   } else if (snsType === "Naver") {
     icon = (
       <S.ImageColumn>
         <Image src="/sns/naver.png" />
       </S.ImageColumn>
     );
-    clickType = naverOauthLogin;
   } else if (snsType === "Google") {
     icon = (
       <S.IconColumn>
         <FcGoogle />
       </S.IconColumn>
     );
-    clickType = googleOauthLogin;
   }
 
   return (
     <S.Container snsType={snsType}>
-      <Button handleOnClick={clickType}>
+      <Button handleOnClick={oauthLink}>
         <S.ColumnWrapper>
           {icon}
           <S.LabelColumn>
