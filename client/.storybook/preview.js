947ab5f69e2960434c1267ddd6a8cca38bbd5b98
hhjin015 <heo015414@gmail.com>
2021-06-17 00:15:31 +0900

    feature Storybook에 Provider 적용

--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -1,18 +1,23 @@
 import React from "react";
-import { ThemeProvider } from 'styled-components';
+
+import { ThemeProvider } from "styled-components";
+
+import { Provider } from "react-redux";
+import store from "../reducer";
+
 import { MINIMAL_VIEWPORTS } from "@storybook/addon-viewport";
+
 import theme from "./theme";
 import Reset from "./reset";
 
 export const decorators = [
   (Story) => (
-    <>
+    <Provider store={store}>
       <Reset />
       <ThemeProvider theme={theme}>
-
         <Story />
       </ThemeProvider>
-    </>
+    </Provider>
   ),
 ];
 export const parameters = {
@@ -20,4 +25,4 @@ export const parameters = {
   viewport: {
     viewports: MINIMAL_VIEWPORTS,
   },
-}
+};
