209b85f55f2a48cbb9b74439f554e959483161f2
hhjin015 <heo015414@gmail.com>
2021-08-18 20:08:02 +0900

    #feature 학번으로 활동 조회 연결, 렌더링

--- client/reducer/student.js
+++ client/reducer/student.js
@@ -1,17 +1,22 @@
-import { fetchStudents, fetchAppendStudent } from "../services/studentService";
+import {
+  fetchStudents,
+  fetchAppendStudent,
+  fetchActivity,
+} from "../services/studentService";
 
 // initialState
 const initialState = {
   students: [],
   id: 17,
   last: false,
+  activity: [],
 };
 
 // action type
 const SET_STUDENT = "client/student/SET_STUDENT";
 const SET_APPEND_STUDENT = "client/student/SET_APPEND_STUDENT";
 const SET_LAST = "client/student/SET_LAST";
-
+const SET_ACTIVITY = "client/student/SET_ACTIVITY";
 // reducer
 export const reducer = (state = initialState, action) => {
   if (action.type === SET_STUDENT) {
@@ -36,6 +41,13 @@ export const reducer = (state = initialState, action) => {
       last: last,
     };
   }
+  if (action.type === SET_ACTIVITY) {
+    const { activity } = action;
+    return {
+      ...state,
+      activity: activity,
+    };
+  }
   return state;
 };
 
@@ -45,16 +57,6 @@ export const setStudent = (students, id) => {
     type: SET_STUDENT,
     students,
     id,
-  };
-};
-
-export const loadStudent = (id) => {
-  return async (dispatch) => {
-    const students = await fetchStudents(id);
-
-    const { last, content } = students;
-    dispatch(setLast(last));
-    dispatch(setStudent(content, id));
   };
 };
 
@@ -72,6 +74,23 @@ export const setLast = (last) => {
   };
 };
 
+export const setActivity = (activity) => {
+  return {
+    type: SET_ACTIVITY,
+    activity,
+  };
+};
+
+export const loadStudent = (id) => {
+  return async (dispatch) => {
+    const students = await fetchStudents(id);
+
+    const { last, content } = students;
+    dispatch(setLast(last));
+    dispatch(setStudent(content, id));
+  };
+};
+
 export const loadAppendStudent = (count, number) => {
   return async (dispatch) => {
     const moreStudents = await fetchAppendStudent(count, number);
@@ -81,5 +100,13 @@ export const loadAppendStudent = (count, number) => {
       dispatch(setLast(last));
     }
     dispatch(setAppendStudent(content));
+  };
+};
+
+export const loadActivity = (studentId) => {
+  return async (dispatch) => {
+    const activity = await fetchActivity(studentId);
+
+    dispatch(setActivity(activity));
   };
 };
--- client/services/studentService.js
+++ client/services/studentService.js
@@ -2,7 +2,6 @@ import API from "../utils/api";
 
 export const fetchStudents = async (id) => {
   const { data } = await API.get(`/student-service/students/${id}?size=10`);
-  // const { content } = data;
 
   // console.log("content: ", content instanceof Array);
   return data;
@@ -13,8 +12,13 @@ export const fetchAppendStudent = async (studentId, pageQuery) => {
     `/student-service/students/${studentId}?page=${pageQuery}&size=10`
   );
 
-  // console.log(last);
-  // console.log(content);
+  return data;
+};
+
+export const fetchActivity = async (studentId) => {
+  const { data } = await API.get(
+    `/student-service/students/activity/${studentId}`
+  );
 
   return data;
 };
